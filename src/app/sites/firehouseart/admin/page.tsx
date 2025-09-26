e3b66e1ded4f0c1eefeb6636aa545a9e6545d4d2
paulhartman1 <paulhartman.bassist@gmail.com>
2025-09-26 14:28:38 -0600

    initial

---
+++ src/app/sites/firehouseart/admin/page.tsx
@@ -0,0 +1,8 @@
+    // your-nextjs-project/app/dashboard/page.tsx
+    export default function DashboardPage() {
+      return (
+        <div>
+          <h1>Welcome to the Firehouse admin page!</h1>
+        </div>
+      );
+    }
---
+++ src/app/sites/firehouseart/page.tsx
@@ -0,0 +1,9 @@
+    // your-nextjs-project/app/dashboard/page.tsx
+    export default function DashboardPage() {
+      return (
+        <div>
+          <h1>Welcome to the Firehouse!</h1>
+          <p>This is your new Next.js page.</p>
+        </div>
+      );
+    }
