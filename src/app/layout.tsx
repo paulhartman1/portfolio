d3eb0aa52c4871f073a4b3b5039397afbe35ee38
paulhartman1 <paulhartman.bassist@gmail.com>
2026-07-06 16:55:05 -0600

    GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 import "./globals.css";
 
@@ -38,7 +39,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-       
+        <GoogleAnalytics />
         {children}
       </body>
     </html>
